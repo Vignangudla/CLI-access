9e3012e69c85082f71ea767fabb744ad51ca4e25
Vignangudla <147783291+Vignangudla@users.noreply.github.com>
2025-05-18 19:05:33 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,8 @@
-@echo off 
-import unittest 
-from src.main import print 
-class TestMain(unittest.TestCase): 
-    def test_print(self): 
-        self.assertTrue(True) 
-if __name__ == "__main__": 
-    unittest.main() 
+import unittest
+
+class TestMain(unittest.TestCase):
+    def test_dummy(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
